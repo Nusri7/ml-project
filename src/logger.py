3a09544a705c2ab8f822314f835387815fd03670
Nusri7 <321nusri@gmail.com>
2023-07-20 19:46:49 +0530

    data_transformation

--- src/logger.py
+++ src/logger.py
@@ -4,10 +4,9 @@ from datetime import datetime
 
 LOG_FILE = f"{datetime.now().strftime('%Y%m%d%H%M%S')}.log"
 log_path = os.path.join(os.getcwd(), "logs", LOG_FILE)
-os.makedirs(os.path.dirname(log_path), exist_ok=True)
+os.makedir(os.path.dirname(log_path))
 
 LOG_FILE_PATH = os.path.join(log_path, LOG_FILE)
-
 logging.basicConfig(filename=log_path, level=logging.INFO,
                     format="%(asctime)s:%(levelname)s:%(message)s")
 
