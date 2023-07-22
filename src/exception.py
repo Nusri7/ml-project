8bc5174d8f0b34ebdbd95115e1ee9b2c3b4b81cf
Nusri7 <321nusri@gmail.com>
2023-07-22 06:40:40 +0530

    Model Train

--- src/exception.py
+++ src/exception.py
@@ -1,19 +1,23 @@
 import sys
+sys.path.insert(0 , 'src/')
+from logger import logging
 
-def error_message_detail(error,error_detail):
-    _,_exc_tb=error_detail.exc_info()
-    file_name=_exc_tb.tb_frame.f_code.co_filename
-    error_message="Error occured in Python script name [{0}] at line number [{1}] with error message [{2}]".format(file_name,_exc_tb.tb_lineno,error)
+
+def error_message_detail(error,error_detail:sys):
+    _,_,exc_tb=error_detail.exc_info()
+    file_name=exc_tb.tb_frame.f_code.co_filename
+    error_message="Error occured in python script name [{0}] line number [{1}] error message[{2}]".format(
+    file_name,exc_tb.tb_lineno,str(error))
 
     return error_message
 
-class CustomException(Exception):
-    def __init__(self,error,error_detail):
-        super().__init__(error)
-        self.error=error
-        self.error_detail=error_detail
-        self.error_message= error_message_detail(self.error,self.error_detail)
-        sys.exit(1) 
+    
 
+class CustomException(Exception):
+    def __init__(self,error_message,error_detail:sys):
+        super().__init__(error_message)
+        self.error_message=error_message_detail(error_message,error_detail=error_detail)
+    
     def __str__(self):
         return self.error_message
+    
--- src/logger.py
+++ src/logger.py
@@ -4,7 +4,7 @@ from datetime import datetime
 
 LOG_FILE = f"{datetime.now().strftime('%Y%m%d%H%M%S')}.log"
 log_path = os.path.join(os.getcwd(), "logs", LOG_FILE)
-os.makedir(os.path.dirname(log_path))
+os.makedirs(os.path.dirname(log_path), exist_ok=True)
 
 LOG_FILE_PATH = os.path.join(log_path, LOG_FILE)
 logging.basicConfig(filename=log_path, level=logging.INFO,
